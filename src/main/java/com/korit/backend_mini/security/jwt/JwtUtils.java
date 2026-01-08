b8c57350057d0773f10942442fc75ef4099e6176
heewon <heewon@heewonui-MacBookAir.local>
2026-01-08 19:47:45 +0900

    Feat: oauth2

--- src/main/java/com/korit/backend_mini/security/jwt/JwtUtils.java
+++ src/main/java/com/korit/backend_mini/security/jwt/JwtUtils.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.util.Base64;
 import java.util.Date;
 
 @Component
